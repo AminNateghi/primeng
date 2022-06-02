422ff0bc3efb649ade2a2918eb6dc507fcbafe11
Amin Nateghi <amin.programmer@gmail.com>
2022-06-02 14:44:07 +0300

    change menu item data

--- src/app/shared/modules/menu-bar/menu-items.data.ts
+++ src/app/shared/modules/menu-bar/menu-items.data.ts
@@ -6,32 +6,11 @@ export const MENU_ITEMS_DATA: MenuItem[] = [
     icon: 'pi pi-home',
   },
   {
-    label: 'Testing',
-    items: [
-      {
-        label: 'Steps',
-        icon: 'pi pi-book',
-      },
-      {
-        label: 'Open',
-        icon: 'pi pi-fw pi-external-link',
-        disabled: true,
-      },
-      { separator: true },
-      {
-        label: 'Quit',
-        icon: 'pi pi-fw pi-times',
-        disabled: true,
-      }
-    ]
+    label: 'Steps Example',
+    icon: 'pi pi-book',
   },
   {
     label: 'User Manager',
-    icon: 'pi pi-fw pi-user',
-    items: [
-      { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-      { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-    ]
+    icon: 'pi pi-user',
   },
-
 ];
