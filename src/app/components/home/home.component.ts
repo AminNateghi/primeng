ec36817f559ddaaf8543ac7325409f7225d4a8d0
Amin Nateghi <amin.programmer@gmail.com>
2022-06-02 14:43:55 +0300

    added title to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/shared/services/message.service';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit() {
+    this.messageService.pageHeaderState.next({ title: 'Dashboard', subTitle: 'This app for test PrimeNg component' });
+  }
 
 }
