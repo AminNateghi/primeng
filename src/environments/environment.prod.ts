65db393c04bfe41340bbcb2c7c09c5e195e3d71a
Amin Nateghi <amin.programmer@gmail.com>
2022-06-06 00:24:07 +0300

    change env. file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "src/app/shared/models/view-models/environment.vm";
+
+export const environment: Environment = {
+  production: true,
+  apiUrl: 'https://jsonplaceholder.typicode.com',
 };
