509ab5a86634fc28e7ae1b6d262ab55d29e18c4d
Amin Nateghi <amin.programmer@gmail.com>
2022-06-05 20:02:24 +0300

    add reducer to app.module and steps.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutModule } from './shared/layouts/main-layout.module';
+import { profileReducer } from './shared/store/profile.reducer';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { MainLayoutModule } from './shared/layouts/main-layout.module';
     BrowserModule,
     AppRoutingModule,
     MainLayoutModule,
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/steps-page/steps-page.module.ts
+++ src/app/components/steps-page/steps-page.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { routes } from './steps.route';
 import { StepModule } from './steps/steps.module';
+import { StoreModule } from '@ngrx/store';
+import { profileReducer } from 'src/app/shared/store/profile.reducer';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
+    StoreModule.forFeature('profile', profileReducer),
     CommonModule,
+    HttpClientModule,
     StepModule,
   ],
 })
