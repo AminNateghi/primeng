316aa836909f79b3971073fc123281ce02768990
Amin Nateghi <amin.programmer@gmail.com>
2022-06-05 23:25:06 +0300

    added animation to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutModule } from './shared/layouts/main-layout.module';
-import { profileReducer } from './shared/store/profile.reducer';
 
 @NgModule({
   declarations: [
@@ -13,6 +13,7 @@ import { profileReducer } from './shared/store/profile.reducer';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MainLayoutModule,
     StoreModule.forRoot({}),
